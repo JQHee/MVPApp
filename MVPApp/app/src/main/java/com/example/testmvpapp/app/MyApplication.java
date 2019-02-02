ba64035428da1f80e7c68d7f5841815bc3183461
HJQ <v-huangjiaqian@midland.local>
2019-02-02 11:52:19 +0800

    添加几个工具类

--- MVPApp/app/src/main/java/com/example/testmvpapp/app/MyApplication.java
+++ MVPApp/app/src/main/java/com/example/testmvpapp/app/MyApplication.java
@@ -1,6 +1,11 @@
 package com.example.testmvpapp.app;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+
 import com.example.testmvpapp.di.component.AppComponent;
 import com.example.testmvpapp.di.component.DaggerAppComponent;
 import com.example.testmvpapp.di.module.AppModule;
@@ -14,10 +19,23 @@ public class MyApplication extends Application {
     private static MyApplication instance;
     public static AppComponent appComponent;
 
+    //以下属性应用于整个应用程序，合理利用资源，减少资源浪费
+    private static Thread mMainThread;//主线程
+    private static long mMainThreadId;//主线程id
+    private static Looper mMainLooper;//循环队列
+    private static Handler mHandler;//主线程Handler
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        //对全局属性赋值
         instance = this;
+        mMainThread = Thread.currentThread();
+        mMainThreadId = android.os.Process.myTid();
+        mHandler = new Handler();
+
+
         initBugly();
         initJPUSH();
     }
@@ -48,5 +66,46 @@ public class MyApplication extends Application {
         JPushInterface.init(this);
         // 获取极光推送的ID
         registrationId = JPushInterface.getRegistrationID(this);
+    }
+
+    /**
+     * 重启当前应用
+     */
+    public static void restart() {
+        Intent intent = instance.getPackageManager().getLaunchIntentForPackage(instance.getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        instance.startActivity(intent);
+    }
+
+    public static Thread getMainThread() {
+        return mMainThread;
+    }
+
+    public static void setMainThread(Thread mMainThread) {
+        MyApplication.mMainThread = mMainThread;
+    }
+
+    public static long getMainThreadId() {
+        return mMainThreadId;
+    }
+
+    public static void setMainThreadId(long mMainThreadId) {
+        MyApplication.mMainThreadId = mMainThreadId;
+    }
+
+    public static Looper getMainThreadLooper() {
+        return mMainLooper;
+    }
+
+    public static void setMainThreadLooper(Looper mMainLooper) {
+        MyApplication.mMainLooper = mMainLooper;
+    }
+
+    public static Handler getMainHandler() {
+        return mHandler;
+    }
+
+    public static void setMainHandler(Handler mHandler) {
+        MyApplication.mHandler = mHandler;
     }
 }
