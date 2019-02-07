22c5466444e823f1748ed8136b6784bdc7f70d80
huangjiaqian <18775134221@163.com>
2019-02-07 09:40:40 +0800

    增加多文件选择

--- MVPApp/app/src/main/java/com/example/testmvpapp/sections/launcher/LauncherActivity.java
+++ MVPApp/app/src/main/java/com/example/testmvpapp/sections/launcher/LauncherActivity.java
@@ -1,9 +1,13 @@
 package com.example.testmvpapp.sections.launcher;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.example.testmvpapp.R;
 import com.example.testmvpapp.base.BasePresenter;
@@ -26,11 +30,13 @@ import butterknife.OnClick;
 
 public class LauncherActivity extends SimpleActivity implements ITimerListener {
 
+    @BindView(R.id.rl_back_ground)
+    RelativeLayout mrlayout;
     @BindView(R.id.tv_launcher_timer)
     AppCompatTextView mTvLauncherTimer;
 
     private Timer mTimer = null;
-    private int mCount = 5;
+    private int mCount = 3;
 
     @OnClick(R.id.tv_launcher_timer)
     public void onViewClicked() {
@@ -55,9 +61,35 @@ public class LauncherActivity extends SimpleActivity implements ITimerListener {
     private void checkIsShowScroll() {
 
         // LoginInterceptor.interceptor(LauncherActivity.this, "NewFeaturesActivity", null);
+        //跳转
         Intent intent = new Intent(LauncherActivity.this, NewFeaturesActivity.class);
         startActivity(intent);
         finish();
+
+    }
+
+    /**
+     * 动画的使用
+     */
+    private void startAnimation() {
+        //创建动画---ObjectAnimator动画对象
+        //参数一,执行动画.参数二,动画类型.参数三,开始结束位置
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mrlayout, "alpha", 0.2f, 1.0f);
+        //渐变时间
+        objectAnimator.setDuration(800);
+        //启动
+        objectAnimator.start();
+
+        //重点是监听跳转,
+        //用到了设计模式___适配器模式
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
+            //这里面只选择一个onAnimationEnd执行完成,
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+            }
+        });
     }
 
     @Override
@@ -72,7 +104,7 @@ public class LauncherActivity extends SimpleActivity implements ITimerListener {
 
     @Override
     protected void initEventAndData() {
-
+        startAnimation();
     }
 
     @Override
