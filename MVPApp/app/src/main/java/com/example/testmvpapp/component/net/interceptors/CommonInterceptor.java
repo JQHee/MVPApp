026101a031818c69ee995da4746fb52d835c3043
HJQ <v-huangjiaqian@midland.local>
2019-02-13 16:43:33 +0800

    添加公共参数

--- MVPApp/app/src/main/java/com/example/testmvpapp/component/net/interceptors/CommonInterceptor.java
+++ MVPApp/app/src/main/java/com/example/testmvpapp/component/net/interceptors/CommonInterceptor.java
@@ -85,3 +85,34 @@ public class CommonInterceptor {
 
 }
 
+/* 添加公共参数
+private class commonInterceptor  implements Interceptor{
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        //获取原先的请求
+        Request originalRequest = chain.request();
+        //重新构建url
+        HttpUrl.Builder builder = originalRequest.url().newBuilder();
+        //如果是post请求的话就把参数重新拼接一下，get请求的话就可以直接加入公共参数了
+        if(originalRequest.method().equals("POST")){
+            FormBody body = (FormBody) originalRequest.body();
+            for(int i = 0; i < body.size();i++){
+                Log.i("RequestFatory",body.name(i) + "---" + body.value(i));
+                builder.addQueryParameter(body.name(i),body.value(i));
+            }
+        }
+        //这里是我的2个公共参数
+        builder.addQueryParameter("mallId", SharedPreferManager.getInstance().getValue("mallId"))
+                .addQueryParameter("robotNo",SharedPreferManager.getInstance().getValue("robotNo"));
+        //新的url
+        HttpUrl httpUrl = builder.build();
+        Request request = originalRequest.newBuilder()
+                .method(originalRequest.method(),originalRequest.body())
+                .url(httpUrl).build();
+        return chain.proceed(request);
+    }
+}
+*/
+
+
