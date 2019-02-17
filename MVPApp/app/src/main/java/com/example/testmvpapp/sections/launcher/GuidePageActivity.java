a8cf8073ea1e968726e624137e203a2c65470af1
huangjiaqian <18775134221@163.com>
2019-02-17 18:13:57 +0800

    引导页动态添加

---
+++ MVPApp/app/src/main/java/com/example/testmvpapp/sections/launcher/GuidePageActivity.java
@@ -0,0 +1,49 @@
+package com.example.testmvpapp.sections.launcher;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.testmvpapp.R;
+import com.example.testmvpapp.base.SimpleActivity;
+import com.example.testmvpapp.ui.guidepage.GuidePage;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+
+public class GuidePageActivity extends SimpleActivity {
+
+    @BindView(R.id.guidePage)
+    GuidePage guidePage;
+    private Integer[] images = {
+            R.mipmap.launcher_01,
+            R.mipmap.launcher_02,
+            R.mipmap.launcher_03,
+            R.mipmap.launcher_04,
+            R.mipmap.launcher_05
+    };
+
+    @Override
+    protected Object getLayout() {
+        return R.layout.activity_guide_page;
+    }
+
+    @Override
+    protected void initView() {
+
+        //设置图片集合,圆点颜色大小
+        guidePage.setLocalImageResList(Arrays.asList(images)).setOvalIndicator(Color.parseColor
+                ("#00FF00"), Color.parseColor("#FFFFFF"), 10);
+
+        //设置进入按钮（文字，颜色，大小，背景）点击事件
+        guidePage.setOnEntryClickListener("立即体验", Color.parseColor("#000000"), 10, R.drawable
+                .shape_radius_yellow_bg, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GuidePageActivity.this, "进入主界面", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ MVPApp/app/src/main/java/com/example/testmvpapp/ui/guidepage/ImageLoaderInterface.java
@@ -0,0 +1,12 @@
+package com.example.testmvpapp.ui.guidepage;
+
+import android.content.Context;
+import android.view.View;
+
+import java.io.Serializable;
+
+
+public interface ImageLoaderInterface<T extends View> extends Serializable {
+
+    void displayImage(Context context, Object path, T imageView);
+}
